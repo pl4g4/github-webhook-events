b5412f6b5e6e3c1017e7a795d26153ca86551021
R0n <riraheta@opentable.com>
2021-07-27 10:47:13 -0500

    remove comments

--- main.go
+++ main.go
@@ -1,9 +1,5 @@
 package main
 
-//Start container: docker-compose up
-//Testing curl: curl -X POST http://localhost:8080/github-events -d "TestingThisNewStuff"
-//Review your logs inside container: docker exec -it githubWebhookEvents tail -f githubeventslog.log
-
 import (
 	"fmt"
 	"log"
